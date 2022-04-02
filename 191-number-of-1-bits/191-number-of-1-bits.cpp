10212042c9068133b2a7dedaf2586d9f21f7852e
Rishabh soni <80063042+RishabhSoni01@users.noreply.github.com>
2022-04-02 22:39:07 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (96.47%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        
+        int count =0 ;
+        while(n){
+             if(n&1==1){
+                 count++;
+                 n=n>>1;
+             }
+            else{n=n>>1;}
+        }
+        return count; 
+    }
+};
