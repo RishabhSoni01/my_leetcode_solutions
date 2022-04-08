0843dc7e666d8aa3e62d6172cf4b8cea8f4d463c
Rishabh soni <80063042+RishabhSoni01@users.noreply.github.com>
2022-04-08 21:44:17 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (79.35%) - LeetHub

---
+++ 868-binary-gap/868-binary-gap.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int count=0;
+        int m = INT_MIN;
+        while(n!=0){
+            if(n%2==1){
+                count = max(m,count);
+                m =0;
+            }
+            m +=1;
+            n /=2;
+        }
+        return count;
+    }
+};
