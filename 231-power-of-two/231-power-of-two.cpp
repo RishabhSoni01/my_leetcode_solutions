da44b162b6302eef480f6c479d8eaeebf3bab571
Rishabh soni <80063042+RishabhSoni01@users.noreply.github.com>
2022-04-11 22:17:26 +0530

    Time: 10 ms (8.31%), Space: 6 MB (25.72%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -4,9 +4,9 @@ public:
         if(n==1){
               return true;
         }
-        else if(n%2!=0 || n==0){
+        else if(n&1!=0 || n==0){
             return false;
         }
-        return isPowerOfTwo(n/2);
+        return isPowerOfTwo(n>>1);
      }
 };
