2b4f98ac291c0078d577bfc40d0eec1236205b1e
Rishabh soni <80063042+RishabhSoni01@users.noreply.github.com>
2022-05-30 19:57:10 +0530

    Time: 20 ms (24.73%), Space: 7.2 MB (78.73%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int n = s.length();
+        unordered_map<char,int> m;
+        for(int i=0;i<n;i++){
+            m[s[i]-'a']++;
+            m[t[i]-'a']--;
+        }
+        for(auto x:m){
+            if(x.second)
+                return false;
+        }
+         return true;
+    }
+};
