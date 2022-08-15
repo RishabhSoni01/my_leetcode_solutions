eabca61d0df028bf654eda5e0c64a77179d08a52
Rishabh soni <80063042+RishabhSoni01@users.noreply.github.com>
2022-08-15 22:33:29 +0530

    Time: 3 ms (93.24%), Space: 6.7 MB (43.10%) - LeetHub

--- 38-count-and-say/38-count-and-say.cpp
+++ 38-count-and-say/38-count-and-say.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     string countAndSay(int n) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         if(n==1){
             return "1";
         }
@@ -10,12 +12,12 @@ public:
         string s="11";
         for(int i=3;i<=n;i++){
             string t="";
-            s = s+"@";
+            s +="@";
             int count=1;
             for(int j=1;j<s.length();j++){
                 if(s[j]!=s[j-1]){
-                    t = t+to_string(count);
-                    t = t+s[j-1];
+                    t +=count+'0';
+                    t +=s[j-1];
                     count=1;
                 }
                 else{
