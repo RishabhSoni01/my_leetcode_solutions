faf38196d9dc454de9c6cf15c2d721b61aa695a5
Rishabh soni <80063042+RishabhSoni01@users.noreply.github.com>
2022-08-17 22:44:01 +0530

    Time: 6 ms (10.92%), Space: 6.3 MB (40.14%) - LeetHub

---
+++ 1221-split-a-string-in-balanced-strings/1221-split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int ans=0;
+        int count=0;
+        for(int i=0;i<s.size();i++){
+            if(s[i]=='L'){
+                count+=1;
+            }
+            else{count-=1;}
+            if(count==0)ans+=1;
+        }
+        return ans;
+    }
+};
