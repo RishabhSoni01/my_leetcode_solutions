f7d32b6372bca6e1b29e05d03533546045476896
Rishabh soni <80063042+RishabhSoni01@users.noreply.github.com>
2023-06-17 13:04:48 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -11,24 +11,43 @@ class Solution
     vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
         // Code here
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        //priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        set<pair<int,int>> st;
         vector<int> distance(V,1e9);
         
         distance[S]=0;
-        pq.push({0,S});
-        while(!pq.empty()){
-            int dis=pq.top().first;
-            int node=pq.top().second;
-            pq.pop();
-            for(auto it:adj[node]){
-                int edgeweight=it[1];
-                int adjnode=it[0];
+        //pq.push({0,S});
+        st.insert({0,S});
+        while(!st.empty()){
+            auto it=*(st.begin());
+            int dis=it.first;
+            int node=it.second;
+            st.erase(it);
+            for(auto i:adj[node]){
+                int adjnode=i[0];
+                int edgeweight=i[1];
                 if(dis+edgeweight<distance[adjnode]){
+                    if(distance[adjnode]!=1e9){
+                        st.erase({distance[adjnode],adjnode});
+                    }
                     distance[adjnode]=dis+edgeweight;
-                    pq.push({distance[adjnode],adjnode});
+                    st.insert({distance[adjnode],adjnode});
                 }
             }
         }
+        // while(!pq.empty()){
+        //     int dis=pq.top().first;
+        //     int node=pq.top().second;
+        //     pq.pop();
+        //     for(auto it:adj[node]){
+        //         int edgeweight=it[1];
+        //         int adjnode=it[0];
+        //         if(dis+edgeweight<distance[adjnode]){
+        //             distance[adjnode]=dis+edgeweight;
+        //             pq.push({distance[adjnode],adjnode});
+        //         }
+        //     }
+        // }
         return distance;
     }
 };
