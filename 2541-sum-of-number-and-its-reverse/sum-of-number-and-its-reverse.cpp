d41d1befdcc9640ebd2919f8943ef00fd5b79fe6
Rishabh soni <80063042+RishabhSoni01@users.noreply.github.com>
2024-03-24 19:19:10 +0530

    Time: 289 ms (29.69%) | Memory: 9.4 MB (23.44%) - LeetSync

--- 2541-sum-of-number-and-its-reverse/sum-of-number-and-its-reverse.cpp
+++ 2541-sum-of-number-and-its-reverse/sum-of-number-and-its-reverse.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool sumOfNumberAndReverse(int num) {
-        for(int i=0;i<=num;i++){
+        for(int i=num/2;i<=num;i++){
             string x=to_string(i);
             reverse(x.begin(),x.end());
             
